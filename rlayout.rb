4434d65213728791b605e07c7d85695a2e415d85
christoph.heindl <christoph.heindl@af365f7a-2c74-11df-b152-a13f879ef2d7>
2010-05-29 08:23:00 +0000

    switched to importer/exporter naming.

--- rlayout.rb
+++ rlayout.rb
@@ -7,8 +7,8 @@ require 'rlayout/streaming_exception'
 require 'rlayout/vfs_traversal'
 require 'rlayout/vfs_node'
 require 'rlayout/vfs_group'
-require 'rlayout/sources'
-require 'rlayout/sinks'
+require 'rlayout/importers'
+require 'rlayout/exporters'
 
 =begin
 include RLayout
